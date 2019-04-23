c4f5d5cf15563413956763f1225635aed9569a0e
Diana <felix.diana@uabc.edu.mx>
2019-04-22 22:29:04 -0700

    prueba corte

--- src/main/java/mx/ssmxli/food/controller/CorteCajaController.java
+++ src/main/java/mx/ssmxli/food/controller/CorteCajaController.java
@@ -1,6 +1,7 @@
 package mx.ssmxli.food.controller;
 
 import mx.ssmxli.food.constant.ViewConstant;
+import mx.ssmxli.food.entity.Usuario;
 import mx.ssmxli.food.model.CorteCajaModel;
 import mx.ssmxli.food.service.CorteCajaService;
 import org.apache.commons.logging.Log;
@@ -41,6 +42,11 @@ public class CorteCajaController {
     public String addCorteCaja(@ModelAttribute(name = "corteCajaModel")CorteCajaModel corteCajaModel,
                                Model model) throws Exception {
         log.info("Method: addCorteCaja() -- Params: "+corteCajaModel.toString());
+        Usuario recibo = new Usuario();
+        recibo.setId(1);
+        recibo.setNivel("gerente");
+        recibo.setPassword("1111");
+        recibo.setUsuario("prueba");
         if(corteCajaService.addCorteCaja(corteCajaModel) != null){
             model.addAttribute("result", 1);//esto es para que se muestre un mensaje de que se agregó éxitosamente
         }else{
