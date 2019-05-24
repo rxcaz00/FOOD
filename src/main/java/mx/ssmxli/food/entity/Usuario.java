ef63f4b69526147fccc80f827743d1c94c1a19f5
roberto.cabrales@uabc.edu.mx <Paparazzi3107>
2019-05-24 00:24:52 -0700

    subiendo vista de consulta de usuarios, y arreglando liga de control usuarios

--- src/main/java/mx/ssmxli/food/entity/Usuario.java
+++ src/main/java/mx/ssmxli/food/entity/Usuario.java
@@ -27,10 +27,10 @@ public class Usuario {
     private String apellidoMaterno;
      */
 
-    @OneToMany(mappedBy = "elaboro", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "elaboro", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<CorteCaja> corteCajaElaborados;//CorteCaja que este usuario ha elaborado
 
-    @OneToMany(mappedBy = "reviso", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "reviso", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<CorteCaja> corteCajaRevisados;//CorteCaja que este usuario ha revisado
 
     public Usuario(String usuario, String password, String nivel) {
