f05e7b206d1317a1f0477c74c21e2e415bb105cb
roberto.cabrales@uabc.edu.mx <Paparazzi3107>
2019-05-27 14:56:38 -0700

    detalles esteticos de login y bloqueo fallido

--- src/main/java/mx/ssmxli/food/controller/LoginController.java
+++ src/main/java/mx/ssmxli/food/controller/LoginController.java
@@ -10,18 +10,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class LoginController extends Thread {
     @Autowired
     @Qualifier("securityServiceImpl")
     private SecurityService securityService;
+
+    private int c = 0;
+
+    public void run() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+
+        }
+    }
 
     @GetMapping("/login")
     public String login(Model model, String error, String logout) {
         UsuarioModel usuarioModel = new UsuarioModel();
         model.addAttribute("usuarioModel",usuarioModel);
-
-        if (error != null)
-            model.addAttribute("error", "Usuario o contraseña incorrecto");
+        if (error != null) {
+            c++;
+            if(c >= 3) {
+                model.addAttribute("error", "Bloqueado");
+                run();
+                c = 0;
+            }
+            else
+                model.addAttribute("error", "Usuario o contraseña incorrecto");
+        }
 
         if (logout != null)
             model.addAttribute("message", "Has cerrado sesión");
