6d2a6fddfcfee40fb1ff8c504bb0f016ef7be47b
Diana <felix.diana@uabc.edu.mx>
2019-05-27 21:45:55 -0700

    Login

--- src/main/java/mx/ssmxli/food/controller/LoginController.java
+++ src/main/java/mx/ssmxli/food/controller/LoginController.java
@@ -15,30 +15,16 @@ public class LoginController extends Thread {
     @Qualifier("securityServiceImpl")
     private SecurityService securityService;
 
-    private int c = 0;
 
-    public void run() {
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-
-        }
-    }
 
     @GetMapping("/login")
     public String login(Model model, String error, String logout) {
         UsuarioModel usuarioModel = new UsuarioModel();
         model.addAttribute("usuarioModel",usuarioModel);
-        if (error != null) {
-            c++;
-            if(c >= 3) {
-                model.addAttribute("error", "Bloqueado");
-                run();
-                c = 0;
-            }
-            else
-                model.addAttribute("error", "Usuario o contraseña incorrecto");
-        }
+
+        if (error != null)
+            model.addAttribute("error", "Usuario o contraseña incorrecto");
+
 
         if (logout != null)
             model.addAttribute("message", "Has cerrado sesión");
